43c326b081ad83ddb22ada961c291907ad2f9225
Manoop Gopalan <manoop@gmail.com>
2020-05-19 12:51:57 +0200

    first commit with code

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.gcp.pubsub.core.*;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner cli(PubSubTemplate pubSubTemplate) {
+		return (args) -> {
+			pubSubTemplate.subscribe("messages-subscription-1",
+					(msg, ackConsumer) -> {
+						System.out.println(msg.getData().toStringUtf8());
+						ackConsumer.ack();
+					});
+		};
+	}
+
+}
