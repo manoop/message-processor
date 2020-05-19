cb8ad13d9ecd47e40087a79e0b67e3888ae12a5f
Manoop Gopalan <manoop@gmail.com>
2020-05-19 13:36:27 +0200

    To allow the application to run and not exit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.gcp.pubsub.core.*;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+
+	    SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Hit 'Enter' to terminate");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 	@Bean
 	public CommandLineRunner cli(PubSubTemplate pubSubTemplate) {
